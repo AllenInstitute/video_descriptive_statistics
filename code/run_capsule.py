7390424043d858feb2e4357a549f513f5386166e
yavorska-iryna <iryna.yavorska@alleninstitute.org>
2025-04-22 00:44:49 +0000

    debuging

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -7,7 +7,7 @@ import numpy as np
 
 DATA_PATH = utils.get_data_folder(pipeline=True)
 #zarr_paths = np.unique(utils.find_zarr_paths(DATA_PATH))
-zarr_paths = list(DATA_PATH.glob("*/processed_frames.zarr"))
+zarr_paths = list(DATA_PATH.glob("*/*/processed_frames.zarr"))
 print(f'Found {len(zarr_paths)}.')
 def run():
     for zarr_path in zarr_paths[:1]:
